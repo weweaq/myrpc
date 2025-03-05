7b994e01c3d4e45b73ad930538c21a0741df726f
weweaq <1773465183@qq.com>
2025-03-05 22:57:31 +0800

    第4章 添加spi机制

--- yu-rpc-core/src/main/java/com/yupi/yurpc/serializer/SerializerFactory.java
+++ yu-rpc-core/src/main/java/com/yupi/yurpc/serializer/SerializerFactory.java
@@ -1,5 +1,7 @@
 package com.yupi.yurpc.serializer;
 
+import com.yupi.yurpc.spi.SpiLoader;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,6 +13,11 @@ import java.util.Map;
  * @from <a href="https://yupi.icu">编程导航知识星球</a>
  */
 public class SerializerFactory {
+
+    static {
+        // 通过spi 加载所有实现类
+        SpiLoader.loadAll();
+    }
 
     /**
      * 序列化映射（用于实现单例）
@@ -27,14 +34,25 @@ public class SerializerFactory {
      */
     private static final Serializer DEFAULT_SERIALIZER = KEY_SERIALIZER_MAP.get("jdk");
 
+// 这里是通过map静态加载
+//    /**
+//     * 获取实例
+//     *
+//     * @param key
+//     * @return
+//     */
+//    public static Serializer getInstance(String key) {
+//        return KEY_SERIALIZER_MAP.getOrDefault(key, DEFAULT_SERIALIZER);
+//    }
+
+
     /**
-     * 获取实例
+     * 通过spi机制获取实例
      *
      * @param key
      * @return
      */
     public static Serializer getInstance(String key) {
-        return KEY_SERIALIZER_MAP.getOrDefault(key, DEFAULT_SERIALIZER);
+        return SpiLoader.getInstance(Serializer.class, key);
     }
-
 }
