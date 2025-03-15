cd0d1e51cbf474d6bb2e7cdf9ab27ff3e5a03992
weweaq <1773465183@qq.com>
2025-03-15 23:49:43 +0800

    第7章 使用parsetools.RecordParser解决半包和粘包问题

--- yu-rpc-core/src/main/java/com/yupi/yurpc/proxy/ServiceProxy.java
+++ yu-rpc-core/src/main/java/com/yupi/yurpc/proxy/ServiceProxy.java
@@ -15,6 +15,7 @@ import com.yupi.yurpc.registry.RegistryFactory;
 import com.yupi.yurpc.serializer.JdkSerializer;
 import com.yupi.yurpc.serializer.Serializer;
 import com.yupi.yurpc.serializer.SerializerFactory;
+import com.yupi.yurpc.server.tcp.VertxTcpClient;
 import io.vertx.core.Vertx;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.net.NetClient;
@@ -45,52 +46,7 @@ public class ServiceProxy implements InvocationHandler {
             ServiceMetaInfo selectedServiceMetaInfo = getSelectedService(rpcRequest);
 
             // 发送 TCP 请求
-            Vertx vertx = Vertx.vertx();
-            NetClient netClient = vertx.createNetClient();
-            CompletableFuture<RpcResponse> responseFuture = new CompletableFuture<>();
-            Integer servicePort = selectedServiceMetaInfo.getServicePort();
-            System.out.println("try connect to TCP server port: " + servicePort);
-            netClient.connect(servicePort, selectedServiceMetaInfo.getServiceHost(),
-                    result -> {
-                        if (result.succeeded()) {
-                            System.out.println("Connected to TCP server");
-                            io.vertx.core.net.NetSocket socket = result.result();
-                            // 发送数据
-                            // 构造消息
-                            ProtocolMessage<RpcRequest> protocolMessage = new ProtocolMessage<>();
-                            ProtocolMessage.Header header = new ProtocolMessage.Header();
-                            header.setMagic(ProtocolConstant.PROTOCOL_MAGIC);
-                            header.setVersion(ProtocolConstant.PROTOCOL_VERSION);
-                            header.setSerializer((byte) ProtocolMessageSerializerEnum.getEnumByValue(RpcApplication.getRpcConfig().getSerializer()).getKey());
-                            header.setType((byte) ProtocolMessageTypeEnum.REQUEST.getKey());
-                            header.setRequestId(IdUtil.getSnowflakeNextId());
-                            protocolMessage.setHeader(header);
-                            protocolMessage.setBody(rpcRequest);
-                            // 编码请求
-                            try {
-                                Buffer encodeBuffer = ProtocolMessageEncoder.encode(protocolMessage);
-                                socket.write(encodeBuffer);
-                            } catch (IOException e) {
-                                throw new RuntimeException("协议消息编码错误");
-                            }
-
-                            // 接收响应
-                            socket.handler(buffer -> {
-                                try {
-                                    ProtocolMessage<RpcResponse> rpcResponseProtocolMessage = (ProtocolMessage<RpcResponse>) ProtocolMessageDecoder.decode(buffer);
-                                    responseFuture.complete(rpcResponseProtocolMessage.getBody());
-                                } catch (IOException e) {
-                                    throw new RuntimeException("协议消息解码错误");
-                                }
-                            });
-                        } else {
-                            System.err.println("Failed to connect to TCP server");
-                        }
-                    });
-
-            RpcResponse rpcResponse = responseFuture.get();
-            // 记得关闭连接
-            netClient.close();
+            RpcResponse rpcResponse = VertxTcpClient.doRequest(rpcRequest, selectedServiceMetaInfo);
             return rpcResponse.getData();
 
 
--- yu-rpc-core/src/main/java/com/yupi/yurpc/server/tcp/VertxTcpServer.java
+++ yu-rpc-core/src/main/java/com/yupi/yurpc/server/tcp/VertxTcpServer.java
@@ -1,18 +1,16 @@
 package com.yupi.yurpc.server.tcp;
 
+import com.yupi.yurpc.protocol.ProtocolConstant;
 import com.yupi.yurpc.server.HttpServer;
+import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.net.NetServer;
+import io.vertx.core.parsetools.RecordParser;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class VertxTcpServer implements HttpServer {
-
-    private byte[] handleRequest(byte[] requestData) {
-        // 在这里编写处理请求的逻辑，根据 requestData 构造响应数据并返回
-        // 这里只是一个示例，实际逻辑需要根据具体的业务需求来实现
-        System.out.println("Received request: " + new String(requestData));
-        return "Hello, client!".getBytes();
-    }
 
     @Override
     public void doStart(int port) {
@@ -28,11 +26,14 @@ public class VertxTcpServer implements HttpServer {
         // 启动 TCP 服务器并监听指定端口
         server.listen(port, result -> {
             if (result.succeeded()) {
-                System.out.println("TCP server started on port " + port);
+                log.info("TCP server started on port " + port);
             } else {
-                System.err.println("Failed to start TCP server: " + result.cause());
+                log.info("Failed to start TCP server: " + result.cause());
             }
         });
     }
 
+    public static void main(String[] args) {
+        new VertxTcpServer().doStart(8888);
+    }
 }
